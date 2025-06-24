f4dd8407d8af7193efc015efdc9a00334f339caf
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-06-24 20:15:20 +0530

    draggable components lib added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import "./assets/css/animate.css";
+import "./assets/components/Drag";
 import Splash from "./assets/pages/Splash";
+import Topbar from "./assets/pages/Topbar";
+import Subapps from "./assets/pages/Subapps";
 import Dock from "./assets/pages/Dock";
 
 const App = () => {
   return (
     <>
     <Splash />
+    <Topbar />
+    <Subapps />
     <Dock />
     </>
   )
---
+++ src/assets/pages/Topbar.jsx
@@ -0,0 +1,11 @@
+import "../css/topbar.css";
+
+const Topbar = () => {
+    return (
+        <div className="topbar">
+
+        </div>
+    )
+}
+
+export default Topbar;
