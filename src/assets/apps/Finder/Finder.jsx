4af290244c72b757f53c5f4f5b46cae6e31fa2c8
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-07-25 20:47:23 +0530

    files app update

--- src/assets/apps/Finder/Finder.jsx
+++ src/assets/apps/Finder/Finder.jsx
@@ -6,12 +6,12 @@ const Finder = () => {
             you might not find much to do here since the project has been a lot more focused on backend, with the best possible methods of scalability in terms of apps with lots of features such as appcompilers as a way for the future of the project to make external apps installable on the simulator by its future community
         <br /><br />
         What works:
+        <li>The appmenu present at the start of the dock</li>
         <li>The glass UI feel to the whole environment</li>
         <li>The Window functions such as focus and all application handling such as close, minimise and maximise.</li>
         <li>All dock interactions leaving the applauncher</li>
         <li>Topbar Simulation (no interaction) with status changing with the current app focused</li>
         <li><b>A flexible application handling backend along with an installer(visible only in backend) for the future community to post apps for the project</b></li>
-        <li>More features coming..</li>
         </div>
     )
 }
