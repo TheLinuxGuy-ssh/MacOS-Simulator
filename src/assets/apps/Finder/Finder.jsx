224bca5235b67191e867074c1ba5ce333040cfc7
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-07-28 09:50:43 +0530

    finder update

--- src/assets/apps/Finder/Finder.jsx
+++ src/assets/apps/Finder/Finder.jsx
@@ -1,128 +1,54 @@
-import "./style.css";
+import "./style.css"
 
 const Finder = () => {
   return (
     <>
-    <div id='finder'>
-  <div id='toolbar'>
-    <div id='windowname'>
-      <i></i>
-      All My Files
-    </div>
-    <div id='actions'>
-      <div id='back'></div>
-      <div id='next'></div>
-      <div id='iconV'></div>
-      <div id='list'></div>
-      <div id='grid'></div>
-      <div id='coverflow'>
-        <i className='view'></i>
-      </div>
-      <div id='settings'></div>
-      <div id='share'></div>
-      <input results='0' type='search' />
-    </div>
-  </div>
-  <div id='view'>
-    <div id='sidebar'>
-      <ul>
-        <li>
-          <span className='group'>
-            Favorites
-          </span>
-          <ul>
-            <li className='current_page'>
-              <i className='afiles'></i>
-              All My Files
-            </li>
-            <li>
-              <i className='airdrop'></i>
-              AirDrop
-            </li>
-            <li>
-              <i className='apps'></i>
-              Applications
-            </li>
-            <li>
-              <i className='desk'></i>
-              Desktop
-            </li>
-            <li>
-              <i className='docs'></i>
-              Documents
-            </li>
-            <li>
-              <i className='downs'></i>
-              Downloads
-            </li>
-            <li>
-              <i className='mov'></i>
-              Movies
-            </li>
-            <li>
-              <i className='music'></i>
-              Music
-            </li>
-            <li>
-              <i className='pic'></i>
-              Pictures
-            </li>
-          </ul>
-        </li>
-        <li>
-          <span className='group'>
-            Shared
-          </span>
-        </li>
-      </ul>
-    </div>
-    <div id='content'>
-      <div className='folder'>
-        <div className='icon'></div>
-        <span>
-          Desktop
+    <div className="finder-topbar">
+      <div className="finder-topbar-left">
+        <div className="finder-icon-left">
+          
+        </div>
+        <div className="finder-icon-right">
+
+        </div>
+        <span className="folder-name">
+          Home
         </span>
       </div>
-      <div className='folder'>
-        <div className='icon'></div>
-        <span>
-          Documents
-        </span>
-      </div>
-      <div className='folder'>
-        <div className='icon'></div>
-        <span>
-          Downloads
-        </span>
-      </div>
-      <div className='folder'>
-        <div className='icon'></div>
-        <span>
-          Movies
-        </span>
-      </div>
-      <div className='folder'>
-        <div className='icon'></div>
-        <span>
-          Music
-        </span>
-      </div>
-      <div className='folder'>
-        <div className='icon'></div>
-        <span>
-          Pictures
-        </span>
-      </div>
-      <div className='folder'>
-        <div className='icon'></div>
-        <span>
-          Public
-        </span>
+      <div className="finder-topbar-right">
+        <div className="finder-arrangement">
+          <div className="finder-grid">
+
+          </div>
+          <div className="finder-list">
+            
+          </div>
+          <div className="finder-columns">
+
+          </div>
+          <div className="finder-stack">
+            
+          </div>
+        </div>
+        <div className="finder-topbar-other">
+          <div className="finder-share">
+
+          </div>
+          <div className="finder-tag">
+
+          </div>
+          <div className="finder-other">
+
+          </div>
+        </div>
+        <div className="finder-topbar-search">
+          <div className="finder-search">
+            
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-</>
+    </>
   );
 };
 
