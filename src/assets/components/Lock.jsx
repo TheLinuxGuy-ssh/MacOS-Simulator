8ad681b8dd5a975e43502cce10ae0b63e7806468
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-08-06 10:05:25 +0530

    lock page ready

--- src/assets/components/Lock.jsx
+++ src/assets/components/Lock.jsx
@@ -12,6 +12,7 @@ const Lock = () => {
   const [password, setPass] = useState('');
   const [ctime, setTime] = useState(formattedTime);
   const [cdate, setDate] = useState(formattedTime);
+  const [ wrong, setWrong ] = useState(false)
   const navigate = useNavigate();
   const handleChange = (e) => {
   setPass(e.target.value);
@@ -22,7 +23,12 @@ const Lock = () => {
         navigate("/desktop")
         console.log("it is correct")
     } else {
-        console.log("not correct")
+      setWrong(true)
+      setTimeout(() => {
+        setWrong(false)
+        console.log("wrong wrong")
+  }, 1000)
+  console.log("you know what this means...")
     }
   };
   const UpdateTime = () => {
@@ -63,7 +69,7 @@ const Lock = () => {
         </div>
         <form onSubmit={handleSubmit}>
         <input
-          className="lockscreen-password-box"
+          className={`lockscreen-password-box ${wrong ? "wrong-pass" : null}`}
           type="password"
           value={password || ''}
           onChange={handleChange}
