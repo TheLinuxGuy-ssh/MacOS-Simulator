046e0e915fccc439eb270f1c7094295523e94a72
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-08-11 02:15:52 +0530

    digipod app added! applications now fully responsive to content

---
+++ src/assets/apps/Digipod/Digipod.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const Digipod = () => {
+    return (
+        <iframe src="https://digipod-ai-dev.vercel.app/signin" className={"digipod"} frameborder="0"></iframe>
+    )
+}
+
+export default Digipod
--- src/assets/apps/index.js
+++ src/assets/apps/index.js
@@ -3,4 +3,5 @@ export { default as Terminal } from "./Terminal/Terminal";
 export { default as Safari } from "./Safari/Safari";
 export { default as VisualStudioCode } from "./VisualStudioCode/VisualStudioCode";
 export { default as Calculator } from "./Calculator/Calculator";
-export { default as Tips } from "./Tips/Tips"
+export { default as Digipod } from "./Digipod/Digipod";
+export { default as Tips } from "./Tips/Tips";
