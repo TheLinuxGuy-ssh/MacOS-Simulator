45c7b8b67bd5c5c5d95c366cb25eb8287308d6c3
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-08-10 15:43:18 +0530

    added code editor app, not the bed but we can work with it...

---
+++ src/assets/apps/VisualStudioCode/VisualStudioCode.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const VisualStudioCode = () => {
+    return (
+        <iframe src="https://stackblitz.com/edit/node?file=index.js" className={"vscode"} frameborder="0"></iframe>
+    )
+}
+
+export default VisualStudioCode
--- src/assets/apps/index.js
+++ src/assets/apps/index.js
@@ -1,5 +1,6 @@
 export { default as Finder } from "./Finder/Finder";
 export { default as Terminal } from "./Terminal/Terminal";
 export { default as Safari } from "./Safari/Safari";
+export { default as VisualStudioCode } from "./VisualStudioCode/VisualStudioCode";
 export { default as Calculator } from "./Calculator/Calculator";
 export { default as Tips } from "./Tips/Tips"
