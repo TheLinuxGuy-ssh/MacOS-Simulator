0242809de68a771f228bef703ad1c7e1fc616d27
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-08-11 00:25:10 +0530

    vscode testing

--- src/assets/apps/VisualStudioCode/VisualStudioCode.jsx
+++ src/assets/apps/VisualStudioCode/VisualStudioCode.jsx
@@ -2,7 +2,7 @@ import "./style.css";
 
 const VisualStudioCode = () => {
     return (
-        <iframe src="https://stackblitz.com/edit/node?file=index.js" className={"vscode"} frameborder="0"></iframe>
+        <iframe src="https://macos.thelinuxguy.tech/?folder=/config/workspace" className={"vscode"} frameborder="0"></iframe>
     )
 }
 
