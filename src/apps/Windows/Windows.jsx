ed681198364450bf3f6b59930875abe9068436bb
thelinuxguy-ssh <mr.linux.0@protonmail.com>
2025-08-18 21:07:38 +0530

    windows app hehe, really nice app!

---
+++ src/apps/Windows/Windows.jsx
@@ -0,0 +1,21 @@
+import "./style.css";
+
+const Windows = () => {
+    return (
+        <>
+        
+<main className="windows-main">
+  <header className="windows-header">
+    <h1 className="windows-emotion">:(</h1>
+  </header>
+  <p class="windows-description">Your PC ran into a problem and needs to restart.  We're just collecting some error info, and then we'll restart for you.<span role="progress">&nbsp;(0% complete)</span></p>
+  <footer className="windows-footer">
+    <p><small>If you'd like to know more, you can search online later for this error:<span>&nbsp;HAL_INITIALIZATION_FAILED</span></small></p>
+    <p><small>Also, why did you open this window? like just switch from windows already! we literally steal your data most of the time!</small></p>
+  </footer>
+</main>
+        </>
+    )
+}
+
+export default Windows;
--- src/apps/index.js
+++ src/apps/index.js
@@ -6,4 +6,5 @@ export { default as Calculator } from "./Calculator/Calculator";
 export { default as Digipod } from "./Digipod/Digipod";
 export { default as Tips } from "./Tips/Tips";
 export { default as Chess } from "./Chess/Chess";
+export { default as Windows } from "./Windows/Windows";
 // export { default as Music } from "./Music/Music";
